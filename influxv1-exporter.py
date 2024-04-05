5788ac2e1499bc61d5569596556c7d05912f91a1
m4ci3k2 <m4ci3k2@users.noreply.github.com>
2024-04-05 20:25:19 +0200

    Initial revision

---
+++ influxv1-exporter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+from influxdb import InfluxDBClient
+from influxdb.line_protocol import make_lines
+import argparse
+
+__license__ = 'GPL-3.0'
+parser = argparse.ArgumentParser(description='Dumps data from influx series separating tags and fields and writes influx line format. If the same name is sometimes tag and sometimes field --- tag wins.', add_help=False)
+parser.add_argument('-h', '--host', default='localhost')
+parser.add_argument('-P', '--port', default=8086)
+parser.add_argument('-u', '--user', default=None)
+parser.add_argument('-p', '--password', default=None)
+parser.add_argument('-d', '--db', default='NOAA_water_database')
+parser.add_argument('-m', '--measurement', default='h2o_feet')
+args = parser.parse_args()
+
+client = InfluxDBClient(args.host, args.port, args.user, args.password, args.db)
+measurement = args.measurement
+
+tag_result = client.query('SHOW TAG KEYS FROM ' + measurement)
+tags = [i['tagKey'] for i in tag_result.get_points()]
+tags_with_time = tags + ['time']
+
+data_result = client.query('SELECT * FROM ' + measurement)
+
+for point in data_result.get_points():
+    print(make_lines({'points': [{
+        'measurement': measurement,
+        'tags': { k: v for k, v in point.items() if k in tags},
+        'fields': { k: v for k, v in point.items() if k not in tags_with_time},
+        'time': point['time']
+        }]
+        }))
